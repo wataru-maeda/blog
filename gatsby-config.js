e921fa7ded60cfbf2ac3a550b4b2ff58da753599
WataruMaeda <w.maeda.jp@gmail.com>
2020-10-28 23:44:55 -0700

    chore: update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `KISS ブログ`,
+    author: `Wataru Maeda`,
+    description: `シンプルにわかりやすく書いたテックブログ`,
+    siteUrl: `https://kiss-blog.netlify.app/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `wmaeda_ca`,
     },
   },
   plugins: [
@@ -108,7 +108,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // edit below
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `G-WZQ8MEE3P8`,
       },
     },
     {
