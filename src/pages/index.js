83de057ce1fd1633f5afaeaad089d31de529d39a
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2020-10-31 13:21:35 -0700

    feat(blog): add categories & tags to blog post (#1)
    * chore: update config
    
    * feat: tag feature
    
    * chore: replace home
    
    * chore: add categories

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
+import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Button from "../components/button"
+import SearchPosts from "../components/searchPosts"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const { data, navigate, location } = this.props
+    const siteTitle = "KISS ブログ - IT物づくり"
+    const posts = data.allMdx.edges
+    const localSearchBlog = data.localSearchBlog
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Home"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+        <SEO title="All posts" />
+        <Bio />
+        <SearchPosts
+          posts={posts}
+          localSearchBlog={localSearchBlog}
+          navigate={navigate}
+          location={location}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
-        <h1>
-          Hey people{" "}
-          <span role="img" aria-label="wave emoji">
-            👋
-          </span>
-        </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
-        <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
-        </p>
-        <p>Now go build something great!</p>
-        <Link to="/blog/">
-          <Button marginTop="35px">Go to Blog</Button>
-        </Link>
       </Layout>
     )
   }
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    localSearchBlog {
+      index
+      store
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
