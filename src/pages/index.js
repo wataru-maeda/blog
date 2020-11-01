2badfebecaa999336556dc7461a66f4cd3e9a583
WataruMaeda <w.maeda.jp@gmail.com>
2020-11-01 02:08:54 -0800

    chore: updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,28 @@ const styles = {
   },
 }
 
+const whiteColors = {
+  // -webkit-font-smoothing: 'antialiased';
+  bg: "white",
+  post: "white",
+  textNormal: "#282a37",
+  textLink: "#0372eb",
+  shadow: `0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+    0 100px 80px rgba(0, 0, 0, 0.12)`,
+  hr: `hsla(0, 0%, 100%, 0.2)`,
+  backgroundColor: "var(--bg)",
+}
+
+const switchTheme = () => {
+  Object.keys(whiteColors).forEach(key => {
+    const cssKey = `--${key}`
+    const cssVal = whiteColors[key]
+    document.body.style.setProperty(cssKey, cssVal)
+  })
+}
+
 class IndexPage extends React.Component {
   render() {
     const { data, navigate, location } = this.props
@@ -33,6 +55,7 @@ class IndexPage extends React.Component {
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO title="All posts" />
+        <button onClick={switchTheme}>test</button>
         <div style={styles.root}>
           <Post {...props} />
           <Post {...props} />
