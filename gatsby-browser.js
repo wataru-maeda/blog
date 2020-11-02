70300f7e1f414cf261563042101af5ece376459b
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2020-11-02 01:08:31 -0800

    chore(search): separate searching, filtering logic (#3)
    * chore: update to stateless component
    
    * chore: custom router
    
    * chore: add search field
    
    * chore: update path
    
    * chore: refactor bio
    
    * fix: background issue
    
    * chore: update test article

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,11 @@
+import React from "react"
+import { Provider } from "react-redux"
+import store from "./src/utils/store"
+
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+export const wrapRootElement = ({ element }) => {
+  return <Provider store={store}>{element}</Provider>
+}
