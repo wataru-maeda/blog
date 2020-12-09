3bd7a3dae142aa7d4a5aab8fe4bc7716fcef2323
WataruMaeda <w.maeda.jp@gmail.com>
2020-12-09 01:24:45 -0800

    chore: cleanup

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { Provider } from "react-redux"
-import store from "./src/utils/store"
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './src/utils/store'
 
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import 'typeface-montserrat'
+import 'typeface-merriweather'
 
 export const wrapRootElement = ({ element }) => {
   return <Provider store={store}>{element}</Provider>
