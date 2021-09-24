b3f6a82afd03405aa1808db60e3f72689e1eed03
WataruMaeda <w.maeda.jp@gmail.com>
2021-09-23 22:16:18 -0700

    fhore: add loading animation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './src/utils/store'
 import ReactGA from 'react-ga'
 
 // custom typefaces
 import 'typeface-montserrat'
 import 'typeface-merriweather'
+
+import store from './src/utils/store'
+
+export const onInitialClientRender = () => {
+  setTimeout(() => {
+    const loader = document.getElementById('___loader')
+    loader.style.visibility = 'hidden'
+    loader.style.transition = 'visibility 0s 0.4s, opacity 0.4s linear'
+    loader.style.opacity = 0
+  }, 1000)
+}
 
 export const wrapRootElement = ({ element }) => {
   ReactGA.initialize('UA-185111252-1')
